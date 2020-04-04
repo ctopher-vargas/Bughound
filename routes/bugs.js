f41c2f08a3a17894afa14c8dfb17de103276e2d9
Christopher Vargas <ctopher.vargas@gmail.com>
2020-04-03 22:24:35 -0700

    project so far

--- routes/bugs.js
+++ routes/bugs.js
@@ -6,9 +6,26 @@ var connection = require('../db');
 router.get('/', function(req, res, next) {
   res.render('bugs/index');
 });
-
+//callback hell
 router.get('/new', function(req, res, next){
-	res.render('bugs/new'); 	
+	connection.query("SELECT prog_id, program FROM programs;", function(err, programs){
+		if (err) { throw err; }
+		else {
+			connection.query("SELECT emp_id, username FROM employees;", function(err, users){
+				if (err) { throw err; }
+				else{
+					connection.query("SELECT area_id, prog_id, area FROM areas;", function(err, areas){
+						if (err) { throw err; }
+						else {
+							var program_count = Object.keys(programs).length;
+							console.log(program_count);
+							res.render('bugs/new', { programs: programs, users: users, areas: areas, program_count: program_count });
+						}
+					}); 
+				}
+			});
+		}
+	});
 }); 
 
 router.get('/edit', function(req, res, next){
